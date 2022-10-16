605ef6169abc8977d0a42c77ac0a377637b769d4
Maxim <titov3000@mail.ru>
2022-10-16 13:50:20 +0300

    second commit/end

--- js/script.js
+++ js/script.js
@@ -1,38 +1,120 @@
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+/* Задания на урок:
 
-const adv = document.querySelectorAll('.promo__adv img'),
-    poster = document.querySelector('.promo__bg'),
-    genre = poster.querySelector('.promo__genre'),
-    movieList = document.querySelector('.promo__interactive-list');
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-// Удаление рекламных блоков со страницы
-adv.forEach(item => {
-    item.remove();
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+// Код сработает, только тогда, когда DOM структура будет загружена
+document.addEventListener('DOMContentLoaded', () => {
+
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    const adv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        formInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        // Записывает значение нового фильма от пользователя в перменную
+        let newFilm = formInput.value;
+        // Получение boolean значения у чекбокса, любимый/не любимый
+        const favorite = checkbox.checked;
+        // Если newFilm не пуст, условие выполнится.
+        if (newFilm) {
+
+            if (newFilm.length > 21) {
+                //  Проверка длины строки, и в случае если больше 21 символа, добавляем ...
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+            // Проверка чекбокса
+            if (favorite) {
+                console.log("Добавляем любимый фильм")
+            }
+            // Вносим фильм в массив movieDB и сортируем по алфавиту
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+
+            createMovieList(movieDB.movies, movieList);
+            // Очищение формы. 2 Варианта:
+            addForm.reset();
+            // 2й способ, через evt
+            // evt.target.reset();
+        }
+
+    });
+
+    // Удаление рекламных блоков со страницы
+    const deleteAdv = (arr) => {
+        adv.forEach(item => {
+            item.remove();
+        });
+    };
+
+    deleteAdv(adv);
+
+    const makeChanges = () => {
+        // Изменил жанр фильма
+        genre.textContent = 'Драма';
+        // Изменил задний фон постреа
+        poster.style.backgroundImage = 'url("./img/bg.jpg")';
+    };
+
+    makeChanges();
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+
+    // 1-й аргумент, какие фильмы передаем, 2-й куда будем помещать (см.вызов функции)
+    function createMovieList(films, parent) {
+        // Очищение list с фильмами, их сортировка по алфавиту + нумерация
+        parent.innerHTML = ' ';
+        // Сортируем по алфавиту
+        sortArr(films);
+        // Каждый фильм, и номер по порядку
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+            <li class="promo__interactive-item">${i + 1} ${film}
+                <div class="delete"></div>
+            </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                // Рекурсия, вызовывает сама себя, для повторной сортировки
+                createMovieList(films, parent);
+            });
+        });
+    }
+
+    createMovieList(movieDB.movies, movieList);
 });
 
-// Изменил жанр фильма
-genre.textContent = 'Драма';
 
-// Изменил задний фон постреа
-poster.style.backgroundImage = 'url("./img/bg.jpg")';
-
-// Очищение list с фильмами, их сортировка по алфавиту + нумерация
-movieList.innerHTML = ' ';
-movieDB.movies.sort();
-
-// Каждый фильм, и номер по порядку
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-    <li class="promo__interactive-item">${i + 1} ${film}
-        <div class="delete"></div>
-    </li>
-    `;
-});
